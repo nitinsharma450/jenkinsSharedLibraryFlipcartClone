5be44cc486ea65db2f68a6419f7bc48fa7bacdc2
Nitin Sharma <nitinsharma1059@gmail.com>
2026-02-08 14:40:48 +0530

    Refactor call function formatting in dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,5 +1,5 @@
-def call(String imageName,String tag){
-    echo 'Start building Docker image'
-    sh 'docker build -t ${imageName}:${tag} .'
-    echo 'Docker image built successfully'
+def call(String imageName, String tag) {
+    echo "Start building Docker image"
+    sh "docker build -t ${imageName}:${tag} ."
+    echo "Docker image built successfully"
 }
