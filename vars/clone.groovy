155e6f535aa3db2009a501625439d0c35e6d6b2f
Nitin Sharma <nitinsharma1059@gmail.com>
2026-02-08 14:27:30 +0530

    Rename githubClone function to call

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,4 +1,4 @@
-def githubClone(String url, String branch){
+def call(String url, String branch){
    echo 'Fetching code from GitHub'
    git branch:branch, url:url
    echo 'Code cloned successfully'
